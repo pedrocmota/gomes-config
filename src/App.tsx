24a536fc527c307d004f30f09de594cc61a0b11a
pedrocerqueiramota@gmail.com <pedrocerqueiramota@gmail.com>
2022-02-26 16:50:53 -0300

    Primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useMemo} from 'react'
+import SplitPane from 'react-split-pane'
+import {ProcessRaw, processData} from './process'
 
-function App() {
+const App: React.FunctionComponent = () => {
+  const [raw, setRaw] = useState('')
+  const [finalText, setFinalText] = useState('')
+  const [server, setServer] = useState('Selecione um servidor')
+  const [bank, setBank] = useState('')
+
+  const processed = ProcessRaw(raw)
+  const str = processData(processed || [], server, bank)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <SplitPane split="horizontal" minSize={50} defaultSize={300}>
+      <div className="splitpanel">
+        <textarea id="raw" spellCheck="false" onChange={(e) => setRaw(e.target.value)} />
+        <div className="toolbar">
+          <select value={server} onChange={(e) => setServer(e.target.value)}>
+            <option value="Adrinne">Selecione um servidor</option>
+            <option value="Adrinne">Adrinne</option>
+            <option value="Agaton">Agaton</option>
+            <option value="Akkan">Akkan</option>
+            <option value="Aldebaran">Aldebaran</option>
+            <option value="Antares">Antares</option>
+            <option value="Arcturus">Arcturus</option>
+            <option value="Asta">Asta</option>
+            <option value="Avesta">Avesta</option>
+            <option value="Azena">Azena</option>
+            <option value="Beatrice">Beatrice</option>
+            <option value="Bergstrom">Bergstrom</option>
+            <option value="Brelshaza">Brelshaza</option>
+            <option value="Calvasus">Calvasus</option>
+            <option value="Danube">Danube</option>
+            <option value="Elzowin">Elzowin</option>
+            <option value="Enviska">Enviska</option>
+            <option value="Feiton">Feiton</option>
+            <option value="Galatur">Galatur</option>
+            <option value="Gienah">Gienah</option>
+            <option value="Inanna">Inanna</option>
+            <option value="Kadan">Kadan</option>
+            <option value="Karta">Karta</option>
+            <option value="Kazeros">Kazeros</option>
+            <option value="Kharmine">Kharmine</option>
+            <option value="Kurzan">Kurzan</option>
+            <option value="Ladon">Ladon</option>
+            <option value="Mari">Mari</option>
+            <option value="Mokoko">Mokoko</option>
+            <option value="Moonkeep">Moonkeep</option>
+            <option value="Neria">Neria</option>
+            <option value="Nineveh">Nineveh</option>
+            <option value="Petrania">Petrania</option>
+            <option value="Prideholme">Prideholme</option>
+            <option value="Procyon">Procyon</option>
+            <option value="Punika">Punika</option>
+            <option value="Regulus">Regulus</option>
+            <option value="Rethramis">Rethramis</option>
+            <option value="Rohendel">Rohendel</option>
+            <option value="Sasha">Sasha</option>
+            <option value="Sceptrum">Sceptrum</option>
+            <option value="Shadespire">Shadespire</option>
+            <option value="Shandi">Shandi</option>
+            <option value="Sirius">Sirius</option>
+            <option value="Slen">Slen</option>
+            <option value="Stonehearth">Stonehearth</option>
+            <option value="Thaemine">Thaemine</option>
+            <option value="Thirain">Thirain</option>
+            <option value="Tortoyk">Tortoyk</option>
+            <option value="Tragon">Tragon</option>
+            <option value="Trixion">Trixion</option>
+            <option value="Una">Una</option>
+            <option value="Valtan">Valtan</option>
+            <option value="Vern">Vern</option>
+            <option value="Vykas">Vykas</option>
+            <option value="Wei">Wei</option>
+            <option value="Yorn">Yorn</option>
+            <option value="Zinnervale">Zinnervale</option>
+            <option value="Zosma">Zosma</option>
+          </select>
+          <input
+            id="character"
+            placeholder="Nome do banco"
+            value={bank}
+            onChange={(e) => setBank(e.target.value)}
+          />
+          <h1>Cole o texto bruto das contas no campo acima</h1>
+        </div>
+      </div>
+      <div className="splitpanel">
+        <textarea id="final" spellCheck="false" readOnly={true} value={str} />
+      </div>
+    </SplitPane>
+  )
 }
 
-export default App;
+export default App
---
+++ src/process.ts
@@ -0,0 +1,46 @@
+import dedent from 'dedent'
+
+interface IAccount {
+  login: string,
+  password: string
+}
+
+export const ProcessRaw = (raw: string) => {
+  if (raw.length > 0) {
+    const lines = raw.split('\n')
+    const accounts = [] as IAccount[]
+    lines.forEach((line) => {
+      if (line.startsWith('巴西区账号')) {
+        const clean = line
+          .replaceAll('巴西区账号', '')
+          .replaceAll('密码', '')
+          .replaceAll('邮箱账号', '')
+          .replaceAll('邮箱密码', '')
+          .replaceAll('邮箱登录网址', '')
+        const parts = clean.split('：')
+        accounts.push({
+          login: parts[1],
+          password: parts[2]
+        })
+      }
+    })
+    return accounts
+  }
+}
+
+export const processData = (accounts: IAccount[], server: string, bank: string) => {
+  let str = ''
+  accounts.forEach((account) => {
+    str += dedent(`
+    {
+      acc = "${account.login}";
+      psw = "${account.password}";
+      regserver = 3;
+      server = "${server}";
+      mailname = "${bank}";
+      fin = 0;
+    },
+    `) + ' '
+  })
+  return str
+}
